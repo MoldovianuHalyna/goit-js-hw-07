19e6e96873b5f81d2548f911f6c924caa8427d41
Halyna Moldovianu <Beorgeos@gmail.com>
2025-02-21 18:29:59 +0100

    2

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('.body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+function changeColor() {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+}
+
+button.addEventListener('click', changeColor);
